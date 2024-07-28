ea001177d49665f71a5603312a53e7008e8f3eb3
Rohpar1504 <115747566+Rohpar1504@users.noreply.github.com>
2024-07-29 03:57:56 +0400

    check last

--- script.js
+++ script.js
@@ -11,62 +11,61 @@ document.addEventListener("DOMContentLoaded", function() {
             .attr("height", 600);
 
         // Further D3.js visualization code goes here
-
-        // Global Overview Visualization
-
-        d3.csv("data/global_average_data.csv", function(d) {
-            return { year : d3.parseTime(d.year), value : +d.value }
-        }).then(function(data) {
-            // Define dimensions and margins
-            const margin = {top: 20, right: 20, bottom: 30, left: 50},
-                width = 960 - margin.left - margin.right,
-                height = 500 - margin.top - margin.bottom;
-
-            // Define scales
-            const x = d3.scaleTime()
-                .domain(d3.extent(data, d => d.year))
-                .range([0, width]);
-
-            const y = d3.scaleLinear()
-                .domain([0, d3.max(data, d => d.value)])
-                .range([height, 0]);
-
-            // Create SVG element
-            const svg = d3.select("#visualization").append("svg")
-                .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
-                .append("g")
-                .attr("transform", `translate(${margin.left},${margin.top})`);
-
-            // Add X and Y axes
-            svg.append("g")
-                .attr("transform", `translate(0,${height})`)
-                .call(d3.axisBottom(x));
-
-            svg.append("g")
-                .call(d3.axisLeft(y));
-
-            // Add the line
-            svg.append("path")
-                .datum(data)
-                .attr("fill", "none")
-                .attr("stroke", "steelblue")
-                .attr("stroke-width", 2)
-                .attr("d", d3.line()
-                    .x(d => x(d.year))
-                    .y(d => y(d.value))
-                );
-
-            // Optionally, add area fill under the line
-            svg.append("path")
-                .datum(data)
-                .attr("fill", "steelblue")
-                .attr("opacity", 0.3)
-                .attr("d", d3.area()
-                    .x(d => x(d.year))
-                    .y0(height)
-                    .y1(d => y(d.value))
-                );
-        });
     });
 });
 
+// Global Overview Visualization
+
+d3.csv("data/global_average_data.csv", function(d) {
+    return { year : d3.parseTime(d.year), value : +d.value }
+}).then(function(data) {
+    // Define dimensions and margins
+    const margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    // Define scales
+    const x = d3.scaleTime()
+        .domain(d3.extent(data, d => d.year))
+        .range([0, width]);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.value)])
+        .range([height, 0]);
+
+    // Create SVG element
+    const svg = d3.select("#visualization").append("svg")
+        .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Add X and Y axes
+    svg.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x));
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Add the line
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 2)
+        .attr("d", d3.line()
+            .x(d => x(d.year))
+            .y(d => y(d.value))
+        );
+
+    // Optionally, add area fill under the line
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "steelblue")
+        .attr("opacity", 0.3)
+        .attr("d", d3.area()
+            .x(d => x(d.year))
+            .y0(height)
+            .y1(d => y(d.value))
+        );
+});
